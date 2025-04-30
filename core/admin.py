fcd54f7a791b9b49c5954cc91d3c9dbf6af16d23
Jeremy Thompson <jeremythompson.918@gmail.com>
2025-04-29 22:30:37 -0400

    Add BookListing to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import BookListing
+
+
+@admin.register(BookListing)
+class BookListingAdmin(admin.ModelAdmin):
+    pass
