dfc1567a4a6d0f1094683e8207549298b17b9129
Jeremy Thompson <jeremythompson.918@gmail.com>
2025-04-29 22:20:43 -0400

    Create BookListing model
    Issues: #1

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from isbn_field import ISBNField
+
+
+class BookListing(models.Model):
+    # required
+    title = models.CharField(max_length=255)
+    cover_photo = models.FileField(upload_to="book_listing_covers/")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # optional
+    isbn = ISBNField(blank=True)
+
+    def __str__(self):
+        return self.title
