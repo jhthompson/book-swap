cc3c590a777061073531ae3305fb5f5c9048056a
Jeremy Thompson <jeremythompson.918@gmail.com>
2025-05-03 00:05:46 -0400

    Store BookListing created_at timestamp

--- core/models.py
+++ core/models.py
@@ -5,13 +5,14 @@ from isbn_field import ISBNField
 
 
 class BookListing(models.Model):
-    # required
+    # user provided
     title = models.CharField(max_length=255)
     cover_photo = models.FileField(upload_to="book_listing_covers/")
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    # optional
     isbn = ISBNField(blank=True)
+
+    # auto generated
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
