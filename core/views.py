ab6e8b47729856cbb063faadadfbf69c0f5409fb
Jeremy Thompson <jeremythompson.918@gmail.com>
2025-05-02 21:58:14 -0400

    Add GET /listings page
    Issues: #1

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from core.models import BookListing
+
+
+def index(request):
+    context = {}
+
+    return render(request, "core/index.html", context)
+
+
+@login_required
+def listings(request):
+    context = {}
+
+    context["listings"] = BookListing.objects.filter(owner=request.user)
+
+    return render(request, "core/listings.html", context)
