1d8557b8cec522f2213ad05b53eaab5768fa6f9f
Jeremy Thompson <jeremythompson.918@gmail.com>
2025-05-05 21:39:25 -0400

    Remove unused import

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
